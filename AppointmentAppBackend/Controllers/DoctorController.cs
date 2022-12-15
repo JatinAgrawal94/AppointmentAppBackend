d6e51511019e79583219ba2339bcd76121aed8b5
Jatin Agrawal <jatinagrawal0801@gmail.com>
2022-12-15 12:12:07 -0800

    authorization middleware initiated

--- AppointmentAppBackend/Controllers/DoctorController.cs
+++ AppointmentAppBackend/Controllers/DoctorController.cs
@@ -1,5 +1,6 @@
 ﻿using AppointmentAppBackend.Data;
 using AppointmentAppBackend.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -8,6 +9,7 @@ namespace AppointmentAppBackend.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class DoctorController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
